62ab3eb16239f4c233f173e1467625c31f7e13df
Khemmie-Ray <atokemmy@gmail.com>
2025-10-15 22:34:06 +0100

    network setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import { AppKitProvider } from './connection'
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router";
+import Layout from './layout/Layout';
+import Home from './pages/Home';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+    </Route>
+  )
+);
+
 
 const App = () => {
   return (
     <AppKitProvider>
-      
+      <div>
+         <RouterProvider router={router} />
+         </div>
       </AppKitProvider>
   )
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useAppKit, useAppKitAccount } from '@reown/appkit/react'
 
 const Header = () => {
+  const { open } = useAppKit()
+  const { isConnected } = useAppKitAccount()
+
   return (
-    <div>Header</div>
+    <div className='flex justify-between w-[90%] mx-auto my-4 py-8'>
+        <p>Dona</p>
+        {!isConnected ?  <button className='bg-amber-950 py-3 rounded-[21px] text-white px-8'  onClick={() => open()}> Connect Wallet</button> : <appkit-account-button />}
+    </div>
   )
 }
 
